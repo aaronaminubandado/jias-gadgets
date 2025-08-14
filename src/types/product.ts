d1c67bd7441ec02422c61cb68726402d99d7338d
Aaron Bandado <aaron.aminu1@gmail.com>
2025-08-14 23:48:38 +0300

    feat: type (interface) definition

---
+++ src/types/product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  description: string;
+  category: string;
+  rating: number;
+  inStock: boolean;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+export interface Cart {
+  items: CartItem[];
+  total: number;
+  itemCount: number;
+}
