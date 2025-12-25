164d82a6a9ff0214e44318fdecb82bc1794ec556
Aaron Bandado <aaron.aminu1@gmail.com>
2025-12-25 16:01:59 +0200

    refactor: remove CartProvider from Index component to simplify structure

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,14 @@
 import { Header } from '@/components/ecommerce/Header';
 import { ProductGrid } from '@/components/ecommerce/ProductGrid';
-import { CartProvider } from '@/hooks/useCart';
 
 const Index = () => {
   return (
-    <CartProvider>
-      <div className="min-h-screen bg-gradient-background">
-        <Header />
-        <main className="container mx-auto px-4 py-8">
-          <ProductGrid />
-        </main>
-      </div>
-    </CartProvider>
+    <div className="min-h-screen bg-gradient-background">
+      <Header />
+      <main className="container mx-auto px-4 py-8">
+        <ProductGrid />
+      </main>
+    </div>
   );
 };
 
